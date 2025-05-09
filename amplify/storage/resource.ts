774a049877e96387c5634221cf95db0b1e311b49
oshans <oshans@timbertruss.com.au>
2025-05-09 11:02:35 +1000

    added an additional bucket

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,26 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'amplifyTeamDrive'
+  name: 'amplifyTeamDrive',
+  access: (allow) => ({
+    'profile-pictures/{entity_id}/*': [
+      allow.guest.to(['read']),
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ],
+    'picture-submissions/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read', 'write'])
+    ],
+  }),
+  isDefault: true,
+});
+
+
+export const secondStorage = defineStorage({
+  name: 'amplify-test-bucket-oshan-1',
+  access: (allow) => ({
+    '*': [
+      allow.authenticated.to(['read', 'write', 'delete'])
+    ]
+  })
 });
